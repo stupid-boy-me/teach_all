a2b5bf4cf98a2b12e3e0780dd7883c6f6cda45d2
love_me_and_world <56495543+stupid-boy-me@users.noreply.github.com>
2022-12-07 20:28:31 +0800

    Create transform_processing.py

---
+++ pytorch_classifier/MobileNet/MobileNetv3_with_Confusion_Matrix/transform_processing.py
@@ -0,0 +1,15 @@
+from torchvision import transforms
+
+transform = {
+        "train": transforms.Compose([
+            transforms.RandomResizedCrop(480),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        "val": transforms.Compose([
+            transforms.RandomResizedCrop(480),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])}
